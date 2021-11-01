f78c7b678596bf1e134b83a8989e210567c99b13
Thomi Aditya Alhakiim <thomiaditya@gmail.com>
2021-11-01 07:47:04 +0700

    fixing the port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const search = require("./indexing");
 const path = require("path");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
